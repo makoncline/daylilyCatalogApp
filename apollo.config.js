ef67bd18484bc5a2fd1a32cb350ad4b854267934
Benjie Gillam <benjie@jemjie.com>
2019-10-02 14:17:39 +0100

    Setup Apollo's VSCode integration

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: {
-    includes: [`${__dirname}/@app/client/src/**/*.graphql`],
+    includes: ["./client/src/**/*.graphql"],
     service: {
       name: "postgraphile",
       localSchemaFile: `${__dirname}/data/schema.graphql`,
