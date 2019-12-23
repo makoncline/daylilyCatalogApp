4698a7f056c4c72ccdc54aee46536f6881782673
Makon Cline <makoncline@gmail.com>
2019-12-23 09:17:13 -0600

    made new env file to run in dev mode

--- scripts/setup.js
+++ scripts/setup.js
@@ -383,11 +383,18 @@ async function main() {
     // This is the root role for the database`);
     await client.query(
       // IMPORTANT: don't grant SUPERUSER in production, we only need this so we can load the watch fixtures!
-      `CREATE ROLE ${DATABASE_OWNER} WITH LOGIN PASSWORD '${DATABASE_OWNER_PASSWORD}' IN ROLE rds_superuser;`
+      `CREATE ROLE ${DATABASE_OWNER} WITH LOGIN PASSWORD '${DATABASE_OWNER_PASSWORD}' SUPERUSER;`
     );
 
-    //makon added
-    await client.query(`GRANT ${DATABASE_OWNER} TO makon;`);
+    // //for heroku deploy
+    // // This is the root role for the database`);
+    // await client.query(
+    //   // IMPORTANT: don't grant SUPERUSER in production, we only need this so we can load the watch fixtures!
+    //   `CREATE ROLE ${DATABASE_OWNER} WITH LOGIN PASSWORD '${DATABASE_OWNER_PASSWORD}' IN ROLE rds_superuser;`
+    // );
+
+    // //makon added
+    // await client.query(`GRANT ${DATABASE_OWNER} TO makon;`);
 
     // This is the no-access role that PostGraphile will run as by default`);
     await client.query(
