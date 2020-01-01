4356f67248c07e9bb9ce6a8ac5e0fec94c4973fa
Makon Cline <makoncline@gmail.com>
2019-12-31 23:45:17 -0600

    working production docker build of server and master. Deployed on digital ocean droplet through docker hub at app.daylilycatalog.com

--- @app/config/src/index.ts
+++ @app/config/src/index.ts
@@ -5,8 +5,8 @@ const packageJson = require("../../../package.json");
 
 export const fromEmail = '"Daylily Catalog" <no-reply@daylilycatalog.com>';
 export const awsRegion = "us-east-1";
-export const projectName = packageJson.name;
-export const companyName = projectName; // For copyright ownership
+export const projectName = "Daylily Catalog";
+export const companyName = "Makon Cline"; // For copyright ownership
 export const emailLegalText =
   // Envvar here so we can override on the demo website
   process.env.LEGAL_TEXT || "";
