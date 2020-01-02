5e3ed07ab7dbf785f397a4b86af6f7ff66725d1c
Makon Cline <makoncline@gmail.com>
2020-01-02 08:30:52 -0600

    commented out accelerated endpoint

--- @app/client/src/pages/api/sign_s3.tsx
+++ @app/client/src/pages/api/sign_s3.tsx
@@ -1,33 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import AWS from "aws-sdk";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req);
   const region = process.env.AWS_REGION;
   const accessKey = process.env.AWSACCESSKEYID;
   const secretKey = process.env.AWSSECRETKEY;
   const bucket = process.env.BUCKET;
   const fileName = req.query.fileName;
   const fileType = req.query.fileType;
-  console.log(region, accessKey, secretKey, bucket);
   AWS.config.update({
-    accessKeyId: accessKey, // Generated on step 1
-    secretAccessKey: secretKey, // Generated on step 1
-    region: region, // Must be the same as your bucket
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+    region: region,
     signatureVersion: "v4",
   });
   const params = {
-    Bucket: "daylily-catalog-images",
-    Key: fileName,
-    Expires: 5 * 60, // 30 minutes
+    Bucket: bucket,
+    Key: fileName + "." + fileType,
+    Expires: 5 * 60,
     ContentType: `image/${fileType}`,
   };
   const options = {
     signatureVersion: "v4",
-    region: region, // same as your bucket
-    endpoint: new AWS.Endpoint(
-      "daylily-catalog-images.s3-accelerate.amazonaws.com"
-    ),
-    useAccelerateEndpoint: true,
+    region: region,
+    // uncomment the next line to use accelerated endpoint
+    // accelerated endpoint must be turned on in your s3 bucket first
+    // endpoint: new AWS.Endpoint(
+    //   "daylily-catalog-images.s3-accelerate.amazonaws.com"
+    // ),
+    // useAccelerateEndpoint: true,
   };
   // @ts-ignore
   const client = new AWS.S3(options);
