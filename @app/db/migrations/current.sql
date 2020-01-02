fedf986fe79343260b8c42f09e5238d4fcfd3d96
Makon Cline <makoncline@gmail.com>
2020-01-02 08:31:46 -0600

    add grant statement for removed then added img_url column

--- @app/db/migrations/current.sql
+++ @app/db/migrations/current.sql
@@ -3,3 +3,9 @@ alter table app_public.lilies
   drop column if exists img_url,
   add column img_url text[];
 
+grant
+  select,
+  insert (img_url),
+  update (img_url),
+  delete
+on table app_public.lilies to :DATABASE_VISITOR;
