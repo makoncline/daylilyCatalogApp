dd54a9505a147c72bf44cc139f17d82039a5dcc3
Makon Cline <makoncline@gmail.com>
2020-01-04 11:16:20 -0600

    remove unnecessary change any types to correct

--- @app/client/src/pages/api/s3.tsx
+++ @app/client/src/pages/api/s3.tsx
@@ -1,12 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import AWS from "aws-sdk";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const bucket = process.env.BUCKET;
-  const fileName = req.query.fileName;
-  const fileType = req.query.fileType;
-  const params = {
+  const bucket: string = process.env.BUCKET || "";
+  const key: string = req.query.key + "";
+  if (!bucket) {
+    res.json({ success: false, err: "no bucket enviromental var set" });
+    return;
+  }
+  if (!key) {
+    res.json({ success: false, err: "no key set" });
+    return;
+  }
+  const params: AWS.S3.DeleteObjectRequest = {
     Bucket: bucket,
-    Key: fileName + "." + fileType,
+    Key: key,
   };
   const client = getClient();
   const operation = req.query.operation;
@@ -17,18 +24,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   function getClient() {
-    //const region = process.env.AWS_REGION;
     const accessKey = process.env.AWSACCESSKEYID;
     const secretKey = process.env.AWSSECRETKEY;
     AWS.config.update({
       accessKeyId: accessKey,
       secretAccessKey: secretKey,
-      //region: region,
-      //signatureVersion: "v4",
     });
     const options = {
-      //signatureVersion: "v4",
-      //region: region,
       // uncomment the next line to use accelerated endpoint
       // accelerated endpoint must be turned on in your s3 bucket first
       // endpoint: new AWS.Endpoint(
@@ -39,13 +41,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const client = new AWS.S3(options);
     return client;
   }
-  function put(client: any, params: any) {
+  function put(client: AWS.S3, params: AWS.S3.DeleteObjectRequest) {
     const putParams = {
       ...params,
       Expires: 5 * 60,
-      //ContentType: `image/${fileType}`,
     };
-    client.getSignedUrl("putObject", putParams, (err: any, url: any) => {
+    client.getSignedUrl("putObject", putParams, (err, url) => {
       if (err) {
         res.json({ success: false, err });
       } else {
@@ -53,14 +54,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       }
     });
   }
-  function del(client: any, params: any) {
-    console.log("del");
-    client.deleteObject(params, (err: any, data: any) => {
+  function del(client: AWS.S3, params: AWS.S3.DeleteObjectRequest) {
+    client.deleteObject(params, err => {
       if (err) {
-        console.log(err);
         res.json({ success: false, err });
       } else {
-        console.log(data);
         res.json({ success: true });
       }
     });
