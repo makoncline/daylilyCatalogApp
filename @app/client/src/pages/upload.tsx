efd7ec57b12bfaf3da24f5c9164f404461081698
Makon Cline <makoncline@gmail.com>
2020-01-04 12:24:21 -0600

    change to upload an array of file

--- @app/client/src/pages/upload.tsx
+++ @app/client/src/pages/upload.tsx
@@ -7,6 +7,7 @@ const ROOT_URL = process.env.ROOT_URL;
 export default function Upload() {
   const [success, setSuccess] = useState(false);
   const [url, setUrl] = useState("");
+  const [images, setImages] = useState<any[]>([]);
   const Success_message = () => (
     <div style={{ padding: 50 }}>
       <h3 style={{ color: "green" }}>SUCCESSFUL UPLOAD</h3>
@@ -17,45 +18,47 @@ export default function Upload() {
   );
   let uploadInput: any = null;
   const handleChange = (ev: any) => {
-    console.log(ev);
+    const file: any | never = ev.target.files[0];
+    if (!file) return;
+    setImages([...images, file]);
     setSuccess(false);
     setUrl("");
   };
   // Perform the upload
   const handleUpload = () => {
-    let file = uploadInput.files[0];
-    if (!file) return;
-    // Split the filename to get the name and type
-    let fileParts = uploadInput.files[0].name.split(".");
-    let fileName = fileParts[0];
-    let fileType = fileParts[1];
-    console.log("Preparing the upload");
-    axios
-      .get(`${ROOT_URL}/api/s3`, {
-        params: {
-          fileName: fileName,
-          fileType: fileType,
-          operation: "put",
-        },
-      })
-      .then(response => {
-        var url = response.data.url;
-        setUrl(url);
-        console.log("Recieved a signed request " + url);
+    images.forEach(file => {
+      if (!file) return;
+      // Split the filename to get the name and type
+      let fileParts = file.name.split(".");
+      let fileName = fileParts[0];
+      let fileType = fileParts[1];
+      console.log("Preparing the upload", fileName, fileType);
+      axios
+        .get(`${ROOT_URL}/api/s3`, {
+          params: {
+            key: fileName + "." + fileType,
+            operation: "put",
+          },
+        })
+        .then(response => {
+          var url = response.data.url;
+          setUrl(url);
+          console.log("Recieved a signed request " + url);
 
-        axios
-          .put(url, file)
-          .then((result: any) => {
-            console.log("Response from s3", result);
-            setSuccess(true);
-          })
-          .catch(error => {
-            console.log("ERROR " + JSON.stringify(error));
-          });
-      })
-      .catch(error => {
-        console.log(JSON.stringify(error));
-      });
+          axios
+            .put(url, file)
+            .then((result: any) => {
+              console.log("Response from s3", result);
+              setSuccess(true);
+            })
+            .catch(error => {
+              console.log("ERROR " + JSON.stringify(error));
+            });
+        })
+        .catch(error => {
+          console.log(JSON.stringify(error));
+        });
+    });
   };
   const handleDelete = () => {
     let file = uploadInput.files[0];
@@ -67,24 +70,13 @@ export default function Upload() {
     axios
       .get(`${ROOT_URL}/api/s3`, {
         params: {
-          fileName: fileName,
-          fileType: fileType,
+          key: fileName + "." + fileType,
           operation: "delete",
         },
       })
-      // .then(response => {
-      //   var url = response.data.url;
-      //   setUrl(url);
-      //   axios
-      //     .put(url, file)
-      //     .then((result: any) => {
-      //       console.log("Response from s3", result);
-      //       setSuccess(true);
-      //     })
-      //     .catch(error => {
-      //       console.log("ERROR " + JSON.stringify(error));
-      //     });
-      // })
+      .then(() => {
+        console.log("item deleted");
+      })
       .catch(error => {
         console.log(JSON.stringify(error));
       });
@@ -94,6 +86,9 @@ export default function Upload() {
       <div>
         <h1>UPLOAD A FILE</h1>
         {success ? <Success_message /> : null}
+        {images.map((image, i) => (
+          <p key={i}>{image.name}</p>
+        ))}
         <input
           onChange={handleChange}
           ref={ref => {
