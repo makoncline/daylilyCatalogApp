952c74e3e9234a778b36fb81f168f7b8b4a6ceb6
Makon Cline <makoncline@gmail.com>
2020-01-09 15:00:32 -0600

    removed standard home page replaced with catalog

--- @app/client/src/pages/index.tsx
+++ @app/client/src/pages/index.tsx
@@ -1,22 +1,76 @@
-import * as React from "react";
-import { Typography } from "antd";
-const { Title, Paragraph } = Typography;
+import React, { useState } from "react";
 import SharedLayout from "../components/SharedLayout";
+import { useLiliesQuery } from "@app/graphql";
+import { Button, Input } from "antd";
+import Error from "../components/ErrorAlert";
+import Redirect from "../components/Redirect";
+import { ApolloError } from "apollo-client";
+import AddLilyForm from "../components/AddLilyForm";
+import LiliesTable from "../components/LiliesTable";
 
-// Convenience helper
-// const Li = ({ children, ...props }: any) => (
-//   <li {...props}>
-//     <Typography>{children}</Typography>
-//   </li>
-// );
+export default function Catalog() {
+  const { data, loading, error } = useLiliesQuery();
+  const user = data && data.currentUser;
+  const [showAddLilyForm, setShowAddLilyForm] = useState(false);
+  const [updateLily, setUpdateLily] = useState(null);
+  const [formError, setFormError] = useState<Error | ApolloError | null>(null);
+  const [nameFilter, setNameFilter] = useState<any>(null);
 
-export default function Home() {
-  return (
-    <SharedLayout title="Home">
-      <Title data-cy="homepage-header">Welcome to the Daylily Catalog</Title>
-      <Paragraph>
-        Create an account or sign in, then click catalog above to begin.
-      </Paragraph>
-    </SharedLayout>
-  );
+  function handleEdit(lily: any) {
+    setUpdateLily(null);
+    setUpdateLily(lily);
+    setShowAddLilyForm(true);
+  }
+
+  const pageContent = (() => {
+    if (error && !loading) {
+      return <Error error={error} />;
+    } else if (!user && !loading) {
+      return <Redirect href={`/login?next=${encodeURIComponent("/lilies")}`} />;
+    } else if (!user) {
+      return "Loading";
+    } else {
+      const userLilies = user.lilies.nodes;
+      const filteredUserLilies = userLilies.filter((lily: any) =>
+        lily.name.toLowerCase().includes(nameFilter)
+      );
+
+      return (
+        <div>
+          <h1 style={{ textAlign: "center" }}>Your Daylily Catalog</h1>
+          <Button
+            type="primary"
+            onClick={() => {
+              setUpdateLily(null);
+              setShowAddLilyForm(true);
+            }}
+            data-cy="settingslilies-button-addlily"
+            style={{ margin: "auto", marginBottom: "1rem", display: "block" }}
+          >
+            Add daylily
+          </Button>
+          <Input
+            value={nameFilter}
+            onChange={e => setNameFilter(e.target.value)}
+          />
+          <LiliesTable
+            dataSource={nameFilter ? filteredUserLilies : userLilies}
+            handleEdit={handleEdit}
+            searchText={nameFilter}
+          />
+          <AddLilyForm
+            onComplete={() => setShowAddLilyForm(false)}
+            error={formError}
+            setError={setFormError}
+            show={showAddLilyForm}
+            setShow={setShowAddLilyForm}
+            updateLily={updateLily}
+            setUpdateLily={setUpdateLily}
+            user={user}
+          />
+        </div>
+      );
+    }
+  })();
+  return <SharedLayout title="Catalog">{pageContent}</SharedLayout>;
 }
