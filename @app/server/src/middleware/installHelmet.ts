9dafb201313bc1f838b163fc526cb3301152ab24
Makon Cline <makoncline@gmail.com>
2021-01-10 16:11:19 -0500

    add security policies for prod also

--- @app/server/src/middleware/installHelmet.ts
+++ @app/server/src/middleware/installHelmet.ts
@@ -1,20 +1,19 @@
 import { Express } from "express";
 import helmet from "helmet";
 
-const isDevOrTest =
-  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
+// const isDevOrTest =
+//   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
 
+// bug in next requires unsave-eval in content security policy
+// but I just turned it off...
+// to turn back on check out the doc below or starter
+// Dev needs 'unsafe-eval' due to
+// https://github.com/vercel/next.js/issues/14221
 export default function installHelmet(app: Express) {
   app.use(
-    helmet(
-      isDevOrTest
-        ? {
-            // Dev needs 'unsafe-eval' due to
-            // https://github.com/vercel/next.js/issues/14221
-            contentSecurityPolicy: false,
-            permittedCrossDomainPolicies: false,
-          }
-        : {}
-    )
+    helmet({
+      contentSecurityPolicy: false,
+      permittedCrossDomainPolicies: false,
+    })
   );
 }
