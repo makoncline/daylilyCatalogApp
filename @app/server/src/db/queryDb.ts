4d8ed254115e31207ab022f39bbbdab7c123d252
Makon Cline <makoncline@gmail.com>
2021-07-30 16:50:52 -0400

    use query params to prevent sql injection

--- @app/server/src/db/queryDb.ts
+++ @app/server/src/db/queryDb.ts
@@ -1,13 +1,13 @@
 import { Pool } from "pg";
 
-export const queryDb = async (query: string) => {
+export const queryDb = async (query: string, values: any[]) => {
   const pool: Pool = new Pool({
     user: process.env.DATABASE_SUPERUSER,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_SUPERUSER_PASSWORD,
   });
-  const res = await pool.query(query);
+  const res = await pool.query(query, values);
   await pool.end();
   return res;
 };
--- @app/server/src/db/saveStripeSubscription.ts
+++ @app/server/src/db/saveStripeSubscription.ts
@@ -7,7 +7,8 @@ export const saveStripeSubscription = async (
 ) => {
   try {
     return await queryDb(
-      `insert into app_public.stripe_subscriptions (id, user_id, customer_id) values('${id}',${userId}, '${customer_id}') returning id;`
+      `insert into app_public.stripe_subscriptions (id, user_id, customer_id) values($1::text,$2::int, $3::text) returning id;`,
+      [id, userId, customer_id]
     );
   } catch (err) {
     throw new Error(`saveStripeSubscription, ${err}`);
