42441a9859f1b1b56c67705a1bb4762816c965ab
Makon Cline <makoncline@gmail.com>
2022-02-05 16:25:30 -0500

    lint

--- @app/client/src/sentry.client.config.js
+++ @app/client/src/sentry.client.config.js
@@ -2,12 +2,14 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://5cf2826fde7e4b14a2c982449e1b8dc6@o1136137.ingest.sentry.io/6188151',
+  dsn:
+    SENTRY_DSN ||
+    "https://5cf2826fde7e4b14a2c982449e1b8dc6@o1136137.ingest.sentry.io/6188151",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
