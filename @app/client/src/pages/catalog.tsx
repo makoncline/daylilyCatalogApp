d99e314e3e5f7cf15e757e4ac1ce17bad3c273db
Makon Cline <makoncline@gmail.com>
2022-05-20 13:31:33 -0400

    add mobile viewport and fix table scroll

--- @app/client/src/pages/catalog.tsx
+++ @app/client/src/pages/catalog.tsx
@@ -2,6 +2,7 @@ import { ErrorAlert, Lilies, Redirect, SharedLayout } from "@app/components";
 import { useSharedQuery } from "@app/graphql";
 import { loginUrl } from "@app/lib";
 import { NextPage } from "next";
+import Head from "next/head";
 import React from "react";
 
 const Catalog: NextPage = () => {
@@ -10,6 +11,9 @@ const Catalog: NextPage = () => {
   const query = useSharedQuery();
   return (
     <SharedLayout title="Catalog" query={query}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       {data && data.currentUser ? (
         <Lilies />
       ) : loading ? (
